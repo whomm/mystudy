580ba8a1f5ef49cfe4c28fdcb260b8f94c72e730
vipkid <vipkid@bogon.vipkid.work>
2019-02-22 10:59:32 +0800

    add

--- sort.php
+++ sort.php
@@ -36,3 +36,23 @@ qicksort($a,0,count($a)-1);
 var_dump($a);
 
 
+
+function checkstr(string $str)
+{
+    $l = strlen($str);
+    $i=0;
+    $j=$l-1;
+    while($i<$j){
+        if($str[$i++] != $str[$j--]) {
+            return false;
+        }
+    }
+    return true;
+   
+}
+
+var_dump(checkstr("abeba"));
+var_dump(checkstr("abca"));
+var_dump(checkstr("aba"));
+var_dump(checkstr("a"));
+var_dump(checkstr(""));
